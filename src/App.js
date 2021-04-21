f6e32a080148291f2251b7d50150e2992c81ad95
Harits-sl <harits.salsabil@gmail.com>
2021-04-22 05:03:25 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from 'elements/button';
+import React from 'react';
+import './assets/scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Button isDisabled />
+    </div>
+  );
+}
+
+export default App;
